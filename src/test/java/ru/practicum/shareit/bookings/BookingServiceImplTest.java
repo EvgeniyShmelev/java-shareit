7a4cfa1061a4b1d747085b38900141ae36de935d
Evgeniy <jeka408@yandex.ru>
2022-10-05 15:00:14 +0300

    -add. BookingServiceImplTest done

--- src/test/java/ru/practicum/shareit/bookings/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/bookings/BookingServiceImplTest.java
@@ -8,8 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import ru.practicum.shareit.booking.dto.BookingAddDto;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.model.BookingState;
 import ru.practicum.shareit.booking.model.BookingStatus;
 import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.item.dto.item.ItemMapper;
 import ru.practicum.shareit.item.dto.item.ItemUserDto;
 import ru.practicum.shareit.item.model.Item;
@@ -18,13 +20,12 @@ import ru.practicum.shareit.user.dto.UserMapper;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.UserService;
 
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
-
-import java.time.LocalDateTime;
 
 @SpringBootTest
 public class BookingServiceImplTest {
@@ -87,5 +88,108 @@ public class BookingServiceImplTest {
         assertEquals(bookingDto, bookingService.getById(owner.getId(), booking.getId()));
     }
 
+    @Test
+    void getBookingsByUserAllTest() {
+        List<BookingDto> bookings = List.of(bookingDto);
+        assertEquals(bookings, bookingService.getBookingsByUser(booker.getId(), BookingState.ALL, 0, 2));
+    }
+
+    @Test
+    void getBookingsByUserWaitingTest() {
+        List<BookingDto> bookings = List.of(bookingDto);
+        assertEquals(bookings, bookingService.getBookingsByUser(booker.getId(), BookingState.WAITING, 0, 2));
+    }
+
+    @Test
+    void getBookingsByUserRejectedTest() {
+        bookingDto = bookingService.update(owner.getId(), booking.getId(), "false");
+        List<BookingDto> bookings = List.of(bookingDto);
+        assertEquals(bookings, bookingService.getBookingsByUser(booker.getId(), BookingState.REJECTED, 0, 2));
+    }
+
+    @Test
+    void getBookingsByUserPastTest() {
+        LocalDateTime start = LocalDateTime.parse("1000-10-05T01:00");
+        LocalDateTime end = LocalDateTime.parse("1500-10-05T01:00");
+        booking = new Booking(2L, start, end, item, booker, BookingStatus.WAITING);
+        bookingAddDto = modelMapper.map(booking, BookingAddDto.class);
+        bookingDto = bookingService.add(booker.getId(), bookingAddDto);
+        List<BookingDto> bookings = List.of(bookingDto);
+        assertEquals(bookings, bookingService.getBookingsByUser(booker.getId(), BookingState.PAST, 0, 2));
+    }
+
+    @Test
+    void getBookingsByUserFutureTest() {
+        LocalDateTime start = LocalDateTime.parse("2023-10-05T01:00");
+        LocalDateTime end = LocalDateTime.parse("2024-10-05T01:00");
+        booking = new Booking(2L, start, end, item, booker, BookingStatus.WAITING);
+        bookingAddDto = modelMapper.map(booking, BookingAddDto.class);
+        bookingDto = bookingService.add(booker.getId(), bookingAddDto);
+        List<BookingDto> bookings = List.of(bookingDto);
+        assertEquals(bookings, bookingService.getBookingsByUser(booker.getId(), BookingState.FUTURE, 0, 2));
+    }
+
+    @Test
+    void getBookingsByUserIncorrectIdTest() {
+        NotFoundException ex = assertThrows(NotFoundException.class,
+                () -> bookingService.getBookingsByUser(-1L, BookingState.ALL, 0, 2));
+        assertThat(ex.getMessage()).contains("Записи не найдены");
+    }
+
+    @Test
+    void getBookingByOwnerAllTest() {
+        List<BookingDto> bookings = List.of(bookingDto);
+        assertEquals(bookings, bookingService.getBookingByOwner(owner.getId(), BookingState.ALL, 0, 2));
+    }
+
+    @Test
+    void getBookingByOwnerWaitingTest() {
+        List<BookingDto> bookings = List.of(bookingDto);
+        assertEquals(bookings, bookingService.getBookingByOwner(owner.getId(), BookingState.WAITING, 0, 2));
+    }
+
+    @Test
+    void getBookingByOwnerRejectedTest() {
+        bookingDto = bookingService.update(owner.getId(), booking.getId(), "false");
+        List<BookingDto> bookings = List.of(bookingDto);
+        assertEquals(bookings, bookingService.getBookingByOwner(owner.getId(), BookingState.REJECTED, 0, 2));
+    }
+
+    @Test
+    void getBookingByOwnerPastTest() {
+        LocalDateTime start = LocalDateTime.parse("1000-10-05T01:00");
+        LocalDateTime end = LocalDateTime.parse("1500-10-05T01:00");
+        booking = new Booking(2L, start, end, item, owner, BookingStatus.WAITING);
+        bookingAddDto = modelMapper.map(booking, BookingAddDto.class);
+        bookingDto = bookingService.add(booker.getId(), bookingAddDto);
+        List<BookingDto> bookings = List.of(bookingDto);
+        assertEquals(bookings, bookingService.getBookingByOwner(owner.getId(), BookingState.PAST, 0, 2));
+    }
+
+    @Test
+    void getBookingByOwnerFutureTest() {
+        LocalDateTime start = LocalDateTime.parse("2023-10-05T01:00");
+        LocalDateTime end = LocalDateTime.parse("2024-10-05T01:00");
+        booking = new Booking(2L, start, end, item, owner, BookingStatus.WAITING);
+        bookingAddDto = modelMapper.map(booking, BookingAddDto.class);
+        bookingDto = bookingService.add(booker.getId(), bookingAddDto);
+        List<BookingDto> bookings = List.of(bookingDto);
+        assertEquals(bookings, bookingService.getBookingByOwner(owner.getId(), BookingState.FUTURE, 0, 2));
+    }
+
+    @Test
+    void getBookingByOwnerIncorrectIdTest() {
+        NotFoundException ex = assertThrows(NotFoundException.class,
+                () -> bookingService.getBookingByOwner(-1L, BookingState.ALL, 0, 2));
+        assertThat(ex.getMessage()).contains("Записи не найдены");
+    }
+
+    @Test
+    void deleteBookingTest() {
+        bookingService.deleteBooking(1L, 1L);
+        NotFoundException ex = assertThrows(NotFoundException.class,
+                () -> bookingService.getById(owner.getId(), booking.getId()));
+        assertEquals("такого запроса нет в списке", ex.getMessage());
+    }
 
 }
