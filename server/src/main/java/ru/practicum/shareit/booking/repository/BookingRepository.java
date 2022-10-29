525f5725f7071818e419c0452ebecf1f484f0102
Evgeniy <jeka408@yandex.ru>
2022-10-29 18:00:19 +0300

    -refactor. Разбил на 2 части по инструкции вебинара

--- server/src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ server/src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.booking.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +7,6 @@ import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.BookingStatus;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
